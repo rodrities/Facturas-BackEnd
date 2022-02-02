61f98cde0b311b9c4815e7685b2cb4f02b78b3ca
rodrities <rodrities@hotmail.com>
2022-02-02 12:33:32 -0500

    Initial commit
    Proyecto creado con spring boot

---
+++ src/main/java/zytrust/facturas/FacturasApplication.java
@@ -0,0 +1,13 @@
+package zytrust.facturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacturasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zytrust/facturas/FacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package zytrust.facturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
