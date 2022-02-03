7fd53ae0793d4f3d0a8ce4107f772c6fa957ad6d
rodrities <rodrities@hotmail.com>
2022-02-03 14:40:57 -0500

    Creado ConverterConfig
    Se crea la configuraión de los converter para que los ModelMapper sean inicializados correctamente

---
+++ src/main/java/zytrust/facturas/config/ConverterConfig.java
@@ -0,0 +1,11 @@
+package zytrust.facturas.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConverterConfig {
+    @Bean
+    public ModelMapper modelMapper() {return new ModelMapper();}
+}
