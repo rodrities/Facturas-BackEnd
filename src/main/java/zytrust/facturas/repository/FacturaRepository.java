9ca52d52490b5f44dc5974dc275d77869d6da327
rodrities <rodrities@hotmail.com>
2022-02-03 14:44:43 -0500

    Creado FacturaRepository
    Se creo la interfaz FacturaRepository que extiende de GenericRepository

---
+++ src/main/java/zytrust/facturas/repository/FacturaRepository.java
@@ -0,0 +1,9 @@
+package zytrust.facturas.repository;
+
+import org.springframework.stereotype.Repository;
+import zytrust.facturas.model.Factura;
+
+@Repository
+public interface FacturaRepository extends GenericRepository<Factura, String> {
+    
+}
