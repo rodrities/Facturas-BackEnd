4b758854acdad2ff608dc289eef683331ac3f923
rodrities <rodrities@hotmail.com>
2022-02-03 14:39:13 -0500

    Creado utils para factura
    Se creo FacturaConverter para poder convertir las clases

---
+++ src/main/java/zytrust/facturas/util/FacturaConverter.java
@@ -0,0 +1,29 @@
+package zytrust.facturas.util;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import zytrust.facturas.dto.FacturaRequest;
+import zytrust.facturas.dto.FacturaResponse;
+import zytrust.facturas.model.Factura;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class FacturaConverter {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Factura convertFacturaToEntity(FacturaRequest request){
+        return modelMapper.map(request, Factura.class);
+    }
+
+    public FacturaResponse convertFacturaToResponse(Factura factura){
+        return modelMapper.map(factura, FacturaResponse.class);
+    }
+
+    public List<FacturaResponse> convertFacturaToResponse(List<Factura> facturas) {
+        return facturas.stream().map(factura -> modelMapper.map(factura, FacturaResponse.class)).collect(Collectors.toList());
+    }
+}
