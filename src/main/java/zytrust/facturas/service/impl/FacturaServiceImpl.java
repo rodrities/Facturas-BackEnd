cbf0b295d49777b8d5fba4d3ccfb4f79bbdc1f4b
rodrities <rodrities@hotmail.com>
2022-02-16 12:39:13 -0500

    PRUEBA-137 Creacion del metodo Put para clientes

--- src/main/java/zytrust/facturas/service/impl/FacturaServiceImpl.java
+++ src/main/java/zytrust/facturas/service/impl/FacturaServiceImpl.java
@@ -18,7 +18,6 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import zytrust.facturas.controller.FacturaController;
-
 import zytrust.facturas.dto.FacturaDetailsResponse;
 
 import zytrust.facturas.dto.FacturaRequest;
