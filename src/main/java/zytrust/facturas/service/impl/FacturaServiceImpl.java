d1d9c6b762ccbd56ed38b6695fffeda6f69c0302
rodrities <rodrities@hotmail.com>
2022-02-16 12:52:15 -0500

    PRUEBA-178 Crear pantalla de detalle de factura

--- src/main/java/zytrust/facturas/service/impl/FacturaServiceImpl.java
+++ src/main/java/zytrust/facturas/service/impl/FacturaServiceImpl.java
@@ -24,7 +24,6 @@ import zytrust.facturas.dto.FacturaResponse;
 import zytrust.facturas.exception.ResourceNotFoundException;
 import zytrust.facturas.model.Cliente;
 import zytrust.facturas.model.Factura;
-
 import zytrust.facturas.model.ProductoFactura;
 import zytrust.facturas.repository.ClienteRepository;
 import zytrust.facturas.repository.FacturaRepository;
