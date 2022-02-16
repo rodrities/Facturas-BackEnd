6d185ca82a8c9f69fd72bf842d31b7b57eb4bcb9
rodrities <rodrities@hotmail.com>
2022-02-16 12:49:21 -0500

    PRUEBA-173 Query para obtener facturas por cliente

--- src/main/java/zytrust/facturas/service/impl/FacturaServiceImpl.java
+++ src/main/java/zytrust/facturas/service/impl/FacturaServiceImpl.java
@@ -23,7 +23,6 @@ import zytrust.facturas.dto.FacturaRequest;
 import zytrust.facturas.dto.FacturaResponse;
 import zytrust.facturas.exception.ResourceNotFoundException;
 import zytrust.facturas.model.Cliente;
-
 import zytrust.facturas.model.Factura;
 import zytrust.facturas.model.ProductoFactura;
 import zytrust.facturas.repository.ClienteRepository;
